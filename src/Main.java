2cf7de9cac63b3676f2329ed231fe9bf78682170
mororolan <robinlanrui@pku.edu.cn>
2022-06-30 23:27:12 +0800

    refine code structure

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,10 @@
-import team3.passpasspass.VM.controller.CustomerPanel;
-import team3.passpasspass.VM.controller.GUI.TextFactory;
-import team3.passpasspass.VM.controller.MachinerySimulatorPanel;
-import team3.passpasspass.VM.controller.SimulatorControlPanel;
-
-import javax.swing.*;
+import team3.Gryffindor.VM.panel.SimulatorControlPanel;
 
 public class Main {
 
     public static void main(String[] args) {
         boolean loginStatus = false;
-//        CustomerPanel customerPanel = new CustomerPanel("VMCS - Customer Panel");
-        new SimulatorControlPanel("VMCS - Simulator Control Panel",loginStatus);
-//        new NewMaintainerPanel("test",loginStatus);
-//        new MachinerySimulatorPanel("VMCS - Machinery Panel",loginStatus);
+        new SimulatorControlPanel("VMCS - Simulator Control Panel - By Team #3",loginStatus);
     }
 }
 
