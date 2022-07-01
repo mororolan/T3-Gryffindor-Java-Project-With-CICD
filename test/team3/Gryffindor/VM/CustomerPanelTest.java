5e43b89cece65716b3c1bd4c7ed033b3ebcf3daa
LessIsMo <72535835+LessIsMo@users.noreply.github.com>
2022-07-01 21:33:50 +0800

    Update CustomerPanelTest.java

--- test/team3/Gryffindor/VM/CustomerPanelTest.java
+++ test/team3/Gryffindor/VM/CustomerPanelTest.java
@@ -72,7 +72,7 @@ public class CustomerPanelTest {
     @Test
     public void startDispense() {
         CustomerPanel customerPanel1 = new CustomerPanel("VMCS - Customer Panel", true);
-        assertEquals(false,customerPanel.btnTerminate.isEnabled());
+        assertEquals(false,customerPanel1.btnTerminate.isEnabled());
         assertEquals(false,customerPanel.lblStartDispensing.isVisible());
     }
 
